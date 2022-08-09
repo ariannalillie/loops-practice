102abc6ac52cc0db15ae2b5c78cdf509412d00a1
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-09 15:07:23 -0400

    learn js functions

--- plus-three.js
+++
@@ -1,7 +0,0 @@
-const plusThree = (num) => {
-  // Your code goes here :)
-};
-
-console.log(plusThree(1)); // Output: 4
-console.log(plusThree(2)); // Output: 5
-console.log(plusThree(3)); // Output: 6
