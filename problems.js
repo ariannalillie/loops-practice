986f38fa8fcfc43b05aeaebb8ebdc98f061626ce
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-15 10:34:46 -0400

    log between stepper specs

--- problems.js
+++ problems.js
@@ -2,11 +2,6 @@ const logBetween = (lowNum, highNum) => {
   // Define a function logBetween(lowNum, highNum) that will return a new array with every number from lowNum to highNum,
   // inclusive. Inclusive means that the range includes lowNum and highNum.
   // Your code goes here
-  let newArray = [];
-  for (let i = lowNum; i <= highNum; i++) {
-    newArray.push(i);
-  }
-  return newArray;
 };
 
 console.log(logBetween(2, 6));
@@ -14,24 +9,17 @@ console.log(logBetween(2, 6));
 const printFives = (max) => {
   // Write a function printFives(max) that return a new array with the multiples of 5 that are less than max.
   // Your code goes here
-  let newArray = [];
-  for (let i = 0; i < max; i++) {
-    if (i % 5 === 0) {
-      newArray.push(i);
-    }
-  }
-  return newArray;
 };
 
 console.log(printFives(30));
 
 const logBetweenStepper = (min, max, step) => {
   // Write a function logBetweenStepper(min, max, step) that takes in 3 numbers as parameters.
-  // The function should print out numbers between min and max at step intervals.
-  // See the following examples.
-  // Hint: this function only needs to print using console.log it does not need to return.
+  // The function should return a new array with numbers between min and max at step intervals.
   // Your code goes here
 };
+
+console.log(logBetween(0, 10, 3))
 
 module.exports = {
   logBetween,
--- test/problems-specs.js
+++ test/problems-specs.js
@@ -10,10 +10,15 @@ describe("Problems", () => {
       expect(logBetween(5, 10)).to.eql([5, 6, 7, 8, 9, 10]);
     });
   });
-  describe("printFive(max))", () => {
+  describe("printFive(max)", () => {
     it("should return a new array with the multiples of 5 that are less than max", () => {
       expect(printFives(30)).to.eql([0, 5, 10, 15, 20, 25]);
       expect(printFives(20)).to.eql([0, 5, 10, 15]);
+    });
+  });
+  describe("logBetweenStepper(minx, max, step)", () => {
+    it("should return a new array with numbers between min and max at step intervals", () => {
+        expect(logBetweenStepper(1, 10, 2)).to.eql([3,5,7,9])
     });
   });
 });
