e5b8287fcdcc93ebe1bef655df1175f402a5e276
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-15 10:28:45 -0400

    printfives spec

--- problems.js
+++ problems.js
@@ -2,25 +2,33 @@ const logBetween = (lowNum, highNum) => {
   // Define a function logBetween(lowNum, highNum) that will return a new array with every number from lowNum to highNum,
   // inclusive. Inclusive means that the range includes lowNum and highNum.
   // Your code goes here
-  let newArray = []
+  let newArray = [];
   for (let i = lowNum; i <= highNum; i++) {
-    newArray.push(i)
+    newArray.push(i);
   }
-  return newArray
+  return newArray;
 };
 
-console.log(logBetween(2,6))
+console.log(logBetween(2, 6));
 
 const printFives = (max) => {
-  // Write a function printFives(max) that prints out the multiples of 5 that are less than max.
+  // Write a function printFives(max) that return a new array with the multiples of 5 that are less than max.
   // Your code goes here
+  let newArray = [];
+  for (let i = 0; i < max; i++) {
+    if (i % 5 === 0) {
+      newArray.push(i);
+    }
+  }
+  return newArray;
 };
+
+console.log(printFives(30));
 
 const logBetweenStepper = (min, max, step) => {
   // Write a function logBetweenStepper(min, max, step) that takes in 3 numbers as parameters.
   // The function should print out numbers between min and max at step intervals.
   // See the following examples.
-
   // Hint: this function only needs to print using console.log it does not need to return.
   // Your code goes here
 };
@@ -28,5 +36,5 @@ const logBetweenStepper = (min, max, step) => {
 module.exports = {
   logBetween,
   printFives,
-  logBetweenStepper
+  logBetweenStepper,
 };
--- test/problems-specs.js
+++ test/problems-specs.js
@@ -10,4 +10,10 @@ describe("Problems", () => {
       expect(logBetween(5, 10)).to.eql([5, 6, 7, 8, 9, 10]);
     });
   });
+  describe("printFive(max))", () => {
+    it("should return a new array with the multiples of 5 that are less than max", () => {
+      expect(printFives(30)).to.eql([0, 5, 10, 15, 20, 25]);
+      expect(printFives(20)).to.eql([0, 5, 10, 15]);
+    });
+  });
 });
