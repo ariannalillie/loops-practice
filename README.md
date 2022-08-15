e091e6713397bc20744876eea7780f2bd9797ffb
Arianna Johnson <ariannajohnson588@gmail.com>
2022-08-15 07:55:28 -0400

    loops practice readme update

--- README.md
+++ README.md
@@ -1,60 +1,6 @@
-# Learn JavaScript Functions
+# Javascript Loops Practice
 
-When you finish this lesson, you should be able to:
-
-* Describe what a function in JavaScript is
-* Demonstrate how to invoke a function
-* Write a function using function declaration
-* Use the `return` keyword to return a value from a function
-
-## JavaScript Operators
-
-* plus: +
-* minus: -
-* divide: /
-* multiply: *
-* modulo: % (the mmodule operator gives you the remainder of two numbers divded, this can be helpful for finding if a number is even or odd)
-**Example: 15 % 2 = 1**
-
-## How to write a function
-
-A function consists of the following three elements:
-
-* The name of the function.
-* A list of parameters to the function, enclosed in parentheses (( )).
-* The code to be run when this function is run, enclosed in curly braces ({ }).
-
-```JavaScript
-const myFunction = (parameter1, parameter2) => {
-    // some code that makes this thing do a thing
-}
-```
-
-## Printing to the console
-
-You can print things to the console using console.log("Message to print").
-
-```JavaScript
-console.log("Hello my name is Zach and I am learning to code! I am so grateful for this lesson, I would change my last name for Ari")
-
-// Prints: Hello my name is Zach and I am learning to code! 
-// I am so grateful for this lesson, I would change my last name for Ari
-```
-
-## Return
-
-Returning a value
-Now that we know how functions are declared and invoked let's talk about the inside of the function. We'll start with a statement: Every function in JavaScript returns undefined unless otherwise specified.
-
-Now what does that mean? We'll start with a simple example:
-
-```JavaScript
-function sayNumber(number) {
-  console.log(number);
-}
-
-sayNumber(1);  // Prints 1 and returns undefined
-```
+Try using both for loops and while loops for this coding exercise. 
 
 ## While Loops
 
--- problems.js
+++ problems.js
@@ -1,129 +1,25 @@
-const addThree = (num) => {
-  // Woohoo 🎉 Congrats on the beginning of your coding challenge Zach!
-  // For this first function it should return the number passed in plus three
-  // Zach's code here
-  return num + 3;
+const logBetween = (lowNum, highNum) => {
+  // Define a function logBetween(lowNum, highNum) that will print every number from lowNum to highNum,
+  // inclusive. Inclusive means that the range includes lowNum and highNum.
+  // Your code goes here
 };
-console.log(addThree(3));
 
-const divideByThree = (num) => {
-  /* Returns the passed in number argument divided by three. */
-  // Zach's code here
-  return num / 3;
+const printFives = (max) => {
+  // Write a function printFives(max) that prints out the multiples of 5 that are less than max.
+  // Your code goes here
 };
-console.log(divideByThree(3));
 
-const averageOfTwo = (num1, num2) => {
-  /* Returns the average of two numbers, num1 and num2. */
-  // Zach's code here
-  const total = num1 + num2;
-  return total / 2;
-};
-console.log(averageOfTwo(5, 6));
+const logBetweenStepper = (min, max, step) => {
+  // Write a function logBetweenStepper(min, max, step) that takes in 3 numbers as parameters.
+  // The function should print out numbers between min and max at step intervals.
+  // See the following examples.
 
-const averageOfAny = (nums) => {
-  /* Takes in an array of nums [2,3,4,5]. The function should return the average of all of
-    the numbers. */
-  // Zach's code here
-
-  let total = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    total += nums[i];
-  }
-
-  return total / nums.length;
-};
-console.log(averageOfAny([2, 3, 4, 5]));
-
-const doubler = (nums) => {
-  /* Takes an array of numbers and returns a new array where every element of
-    the original array is multiplied by 2. */
-  // Zach's code here
-};
-const combineArrays = (arr1, arr2) => {
-  /* Takes in two arrays of numbers and returns the two arrays combined into
-    a single array. **Hint**: Use the `Array.concat` method but be aware that
-    calling this method won't permanently change, also known as **mutate**,
-    either array. */
-  // Zach's code here
-};
-const wordWithinArray = (word, arr) => {
-  /* Takes in both a word and an array of words as arguments and returns a
-    boolean that returns true if that string is located inside of the array, or
-    false if it does not. Use `Array.indexOf`. */
-  // Zach's code here
-};
-const echo = (str) => {
-  /* Takes in a string and returns that string "echo-ized". E.g. 
-    echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
-    echo("hey"); // => returns "HEY ... hey ... hey"
-    echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
-  // Zach's code here
-};
-const fizzBuzz = (max) => {
-  /* Takes a number, max and returns an array that contains every number from
-    0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
-  // Zach's code here
-};
-const hello = (name) => {
-  /* Takes in a string name and returns a string saying "Hello, " to that name. */
-  // Zach's code here
-};
-const goodbye = (name) => {
-  /* Takes in a string name and returns a string saying "Bye, " to that name. */
-  // Zach's code here
-};
-const isFive = (num) => {
-  /* Takes in a number, num, and returns `true` if a number is equal to 5 and
-    `false` if it is not. */
-  // Zach's code here
-};
-const isOdd = (num) => {
-  /* Takes in a number and returns `true` if the number is odd and returns
-    `false` otherwise. Try writing this with and without `if` statements */
-  // Zach's code here
-};
-const isSubString = (searchString, subString) => {
-  /* Takes in two strings, `searchString` and `subString`. Should return
-    `true` if `subString` is a part of the`searchString`, regardless of upper
-    or lower case, and `false` if otherwise. */
-  // Zach's code here
-};
-const aCounter = (word) => {
-  /* Takes in a word and returns the number of a's within that word. Counts
-    both lowercase (a) and uppercase (A). Your job is to translate the following
-    function to use a `for` loop instead of the `while` loop it is currently
-    using. */
-  /* 
-    let index = 0;
-    let count = 0;
-    while (index < word.length) {
-        let char = word[index];
-        if (char === "a" || char === "A") {
-        count += 1;
-        }
-        index++;
-    }
-    return count;
-    */
-  // Your code here
+  // Hint: this function only needs to print using console.log it does not need to return.
+  // Your code goes here
 };
 
 module.exports = {
-  addThree,
-  divideByThree,
-  averageOfTwo,
-  averageOfAny,
-  doubler,
-  combineArrays,
-  wordWithinArray,
-  echo,
-  fizzBuzz,
-  hello,
-  goodbye,
-  isFive,
-  isOdd,
-  isSubString,
-  aCounter,
+  logBetween,
+  printFives,
+  logBetweenStepper
 };
